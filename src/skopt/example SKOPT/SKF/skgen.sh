55f8cfa9cf7a15f6cb9afb2dedd34d7b5156223e
Stanislav Markov <figaro@hku.hk>
2014-09-27 16:49:38 +0800

    Bug fixes and changes that enable a system with 0 sytemweight to be executed but not evaluated. This permits SKgen to be handled on equal footing as atomic systems. Note that in principle such system can still exchange data with other systems. But it's data output will not be conisdered towards cost. Other changes consern the loggers that are now tidied up. Bug-fix in the 'skipexecution' flag in the Evaluator class __init__. Some other minor bug-fixes.
    Former-commit-id: 0dc576162bb99489b677d2e2652aef35a16f27ea

--- src/skopt/example SKOPT/SKF/skgen.sh
+++ src/skopt/example SKOPT/SKF/skgen.sh
@@ -1,15 +1,15 @@
 #skgen atom Si
 #skgen potcomp Si
 #skgen wavecomp Si
-
+#
 #skgen atom O
-skgen potcomp O
-skgen wavecomp O
-
+#skgen potcomp O
+#skgen wavecomp O
+#
 #skgen twocnt -g 0.2 -s potential Si Si
-skgen twocnt -g 0.2 -s potential O O
-skgen twocnt -g 0.2 -s potential Si O
-
+#skgen twocnt -g 0.2 -s potential O O
+#skgen twocnt -g 0.2 -s potential Si O
+#
 #skgen sktable -d Si Si
 skgen sktable -d O O
-skgen sktable -d Si O
+#skgen sktable -d Si O
