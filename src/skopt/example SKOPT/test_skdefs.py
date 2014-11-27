43344436f00525c75ed32e212c0a5d151c412304
Stanislav Markov <figaro@hku.hk>
2014-11-27 09:48:39 +0800

    Some updates to the example notebooks, accommodating updates in relevant classes
    Former-commit-id: 2fc6dc2a882893094ebd07e49c995cf0bc4cf057

---
+++ src/skopt/example SKOPT/test_skdefs.py
@@ -0,0 +1,20 @@
+
+compressions = {
+	"Si": Compression(
+		potcomp = "potential",
+		potcomp_parameters = [[4, 3.45], 
+                          [5, 
+                           3.670000 ], 
+                           [4, 7.67]],
+		wavecomp = "potential",
+		wavecomp_parameters = [[4, 3.45], [4, 6.44], [4, 7.67]],
+		),
+	"O": Compression(
+		potcomp = "potential",
+		potcomp_parameters = [[ 4, 4.000000], 
+                          [4, 4.000000]],
+		wavecomp = "potential",
+		wavecomp_parameters = [[12, 3.1], [12, 3.1]],
+		),
+    }
+
