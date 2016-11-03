07c97f009057efea468a3193f535325717027cc4
stanmarkov <figaro@hku.hk>
2016-11-03 08:39:44 +0800

    Tidied up tasks and their test file; testing likely still incomplete for tasks
    Former-commit-id: 111ae41ff9c6fa613542938e7a4092322ce9ec15
    Former-commit-id: d50c5c86994363ee02193aeb9600e5497a05d5d4

--- src/skopt/tasks.py
+++ src/skopt/tasks.py
@@ -2,6 +2,8 @@ import logging
 import os, sys, subprocess
 from subprocess import STDOUT
 
+DEFAULT_PARAMETER_FILE='current.par'
+parameterfile = DEFAULT_PARAMETER_FILE
 
 class RunTask (object):
     
@@ -53,34 +55,36 @@ class RunTask (object):
             os.chdir(topdir)
             raise
 
-class SetTask (RunTask):
+
+class SetTask (object):
     
+    def __init__(self, parfile=DEFAULT_PARAMETER_FILE, wd='.', append=False):
+        self.parfile = parfile
+        self.wd = wd
+
     def __call__(self, parameters, iteration):
-        # Remember top level directory where skopt is invoked
-        # We must make every effort to return here even if task
-        # fails for some reason!
-        topdir = os.getcwd()
-        # Go to task working directory
-        os.chdir(os.path.abspath(self.wd))
-        # Try to execute the task
-        try:
-            self.logger.debug("Running {} in {}...".format(self.cmd, self.wd))
-            # capture the output/error if any, for eventual checks
-            self.out = subprocess.check_output(self.cmd, 
-                                          universal_newlines=True, 
-                                          stderr=self.err)
-            with open(self.outfile, 'w') as fp:
-                fp.write(self.out)
-            self.logger.debug("Complete: output is in {}".format(self.out))
-            # return to caller's directory
-            os.chdir(topdir)
-        except subprocess.CalledProcessError as exc:
-            # report the issue and exit
-            self.logger.critical('{} failed with exit status {}\n'.
-                            format(self.cmd, exc.returncode))
-            self.out = exc.output
-            with open(self.outfile, 'w') as fp:
-                fp.write(self.out)
-            # go back to top dir
-            os.chdir(topdir)
-            raise
+        self.logger.debug("\nSetting parameteres for iteration {} in {}.".
+                format(iteration, self.parfile))
+        parfile = os.path.join(self.wd, self.parfile)
+        with open(parfile, 'w') as fp:
+            fp.writeline('{}\n'.format(iteration))
+            fp.writeline('{}\n'.format(parameters))
+
+
+class GetTask (object):
+    """Wrapper class to declare tasks w/ arguments but calls w/o args"""
+
+    def __init__(self, func, source, destination, *args, **kwargs):
+        self.func   = func
+        self.src    = source
+        self.dst    = destination
+        self.args   = args
+        self.kwargs = kwargs
+
+    def __call__(self, *args, **kwargs):
+        for arg in args:
+            self.args.append(arg)
+        self.kwargs.update(**kwargs)
+        self.func(self.src, self.dst, *self.args, **self.kwargs)
+        
+
--- test/test_tasks.py
+++ test/test_tasks.py
@@ -37,20 +37,18 @@ class TasksParsingTest(unittest.TestCase):
 
     yamldata="""
         tasks:
-            #- set: [exe, workdir, [argslist] # different from run due to
-            #                                 # intrinsic handling of parameters
-            #                                 # at the level of __call__()
-            #- run: [exe, workdir, [argslist] 
-            #- get: [what, [from], [to]]
-            - set: [skdefs, skf, ]
+            #- set: [parfile, workdir, optional_arguments]
+            #- run: [exe, workdir, arguments] 
+            #- get: [what, source, destination, optional_arguments]
+            - set: [current.par, skf, ]
             - run: [skgen, skf, ]
             - run: [bs_dftb, Si, ]
             - run: [bs_dftb, SiO2, ]
-            - get: [get_dftb, Si, ]
-            - get: [get_dftb, SiO2, ]
-            - get: [get_meff, Si/bs, ]
+            - get: [get_dftb, Si, Si]
+            - get: [get_dftb, SiO2, SiO2]
+            - get: [get_meff, Si/bs, Si/bs]
         """
-    taskmapper = {'run': tasks.RunTask, 'set': None, 'get': None}
+    taskmapper = {'run': tasks.RunTask, 'set': tasks.SetTask, 'get': tasks.GetTask}
 
     def test_parsetask(self):
         """Can we parse task declarations successfully"""
@@ -64,15 +62,96 @@ class TasksParsingTest(unittest.TestCase):
                 a1 = _args.pop(0)
                 tasklist.append(self.taskmapper[_t](a0, a1, *_args))
             except TypeError:
-                self.assertEqual(self.taskmapper[_t], None)
+                # end up here if unknown task type, which is mapped to None
+                self.assertEqual(self.taskmapper.get(_t, None), None)
                 pass
+        self.assertTrue(isinstance(tasklist[0], tasks.SetTask))
         cmd = ['skgen', 'bs_dftb', 'bs_dftb']
-        wd = ['skf', 'Si', 'SiO2']
-        for ii, tt in enumerate(tasklist):
+        wd  = ['skf', 'Si', 'SiO2']
+        for ii, tt in enumerate(tasklist[1:4]):
+            self.assertTrue(isinstance(tt, tasks.RunTask))
             self.assertListEqual(tt.cmd, [cmd[ii]])
             self.assertEqual(tt.wd, wd[ii])
+        fun = ['get_dftb', 'get_dftb', 'get_meff']
+        src = ['Si', 'SiO2', 'Si/bs']
+        for ii, tt in enumerate(tasklist[4:]):
+            self.assertTrue(isinstance(tt, tasks.GetTask))
+            self.assertEqual(tt.src, src[ii])
+            self.assertEqual(tt.dst, src[ii])
 
 
+class GetTaskTest(unittest.TestCase):
+    """Does GetTask operate correctly?"""
+
+    def func_1(self, src, dst, *args, **kwargs):
+        assert(isinstance(src, dict))
+        assert(isinstance(dst, dict))
+        assert('key' in src)
+        dst['key'] = src['key']
+        
+    def func_2(self, src, dst, *args, **kwargs):
+        assert(isinstance(src,dict))
+        assert(isinstance(dst,dict))
+        key = args[0]
+        dst[key] = src[key] 
+
+    def func_3(self, src, dst, *args, **kwargs):
+        assert(isinstance(src,dict))
+        assert(isinstance(dst,dict))
+        key = kwargs['query']
+        assert key=='key'
+        dst[key] = src[key] 
+
+    def test_gettasks_noargs(self):
+        """Can we declare and execute a GetTask?"""
+        src = {}
+        dst = {}
+        # declare: pass empty dictionaries
+        tt = tasks.GetTask(self.func_1, src, dst)
+        self.assertEqual(tt.func, self.func_1)
+        self.assertEqual(tt.src, src)
+        self.assertEqual(tt.dst, dst)
+        # update source dictionary
+        src['key'] = True
+        src['other'] = False
+        # call
+        tt()
+        # check destination
+        self.assertTrue(dst['key'])
+
+    def test_gettasks_posargs(self):
+        """Can we declare a GetTask with positional args and execute it?"""
+        src = {}
+        dst = {}
+        # declare: pass empty dictionaries
+        tt = tasks.GetTask(self.func_2, src, dst, 'key')
+        self.assertEqual(tt.func, self.func_2)
+        self.assertEqual(tt.src, src)
+        self.assertEqual(tt.dst, dst)
+        # update source dictionary
+        src['key'] = True
+        src['other'] = False
+        # call
+        tt()
+        # check destination
+        self.assertTrue(dst['key'])
+
+    def test_gettasks_kwargs(self):
+        """Can we declare a GetTask with keyword args and execute it?"""
+        src = {}
+        dst = {}
+        # declare: pass empty dictionaries
+        tt = tasks.GetTask(self.func_3, src, dst, query='key')
+        self.assertEqual(tt.func, self.func_3)
+        self.assertEqual(tt.src, src)
+        self.assertEqual(tt.dst, dst)
+        # update source dictionary
+        src['key'] = True
+        src['other'] = False
+        # call
+        tt()
+        # check destination
+        self.assertTrue(dst['key'])
 
 if __name__ == '__main__':
     unittest.main()
