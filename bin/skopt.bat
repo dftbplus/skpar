06e1669bf2ba4b0633e4f7859f3270a711631452
stanmarkov <figaro@hku.hk>
2017-01-15 17:14:19 +0800

    Forgot to ci the executable in last commit
    Former-commit-id: 5bc0737b1a47f1fcab8bee9609ac61c161f10e3d
    Former-commit-id: 06e0c0d41811c23060cc26f2518cb11f46c10d77

--- bin/skopt.bat
+++ bin/skopt.bat
@@ -2,4 +2,5 @@
 rem Use python to execute the python script having the same name as this batch
 rem file, but without any extension, located in the same directory as this
 rem batch file
-#!/usr/bin/env python3 "%~dpn0" %*
+rem #!/usr/bin/env python3 "%~dpn0" %*
+\Users\admin\anaconda3\python.exe "%~dpn0" %*
