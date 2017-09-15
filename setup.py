9ce1b7d1cf33bdedd36f0862f316efab3d576a11
Stanislav Markov <figaro@hku.hk>
2017-09-15 13:39:13 +0800

    Updated setup.py to include install_requires, enabling pip install witout -r requirements.txt

--- setup.py
+++ setup.py
@@ -3,25 +3,20 @@
 # Copyright (C) 2013 - 2017  Stanislav Markov
 # Please see the accompanying LICENSE file for further information.
 
-#from distutils.core import setup, Command
-#from distutils.command.build_py import build_py as _build_py
-#from glob import glob
 from setuptools import setup
-
 from os.path import join
-
 import os
 import sys
 
 if sys.version_info < (3, 0, 0, 'final', 0):
     raise SystemExit('Python 3 is required!')
 
+name         = 'skpar'
 short_description = ('Optimisation of Slater-Koster files (.skf) '+
                     'for density functional-based tight binding (DFTB)')
 long_description  = open('README.txt').read()
-name         = 'skpar'
-# todo: try to regenerate ./skpar/__init__.py to include the version stated here
-version      = '0.2'
+
+version      = '0.2.4'
 
 package_dir  = {'skpar': 'skpar',}
 
@@ -48,9 +43,9 @@ setup(name=name,
       description=short_description,
       long_description=long_description,
       url='https://github.com/smarkov/skpar',
-      download_url = 'https://github.com/smarkov/skpar/archive/0.2.3.tar.gz',
-      maintainer='Stanislav Markov, The University of Hong Kong',
-      maintainer_email='figaro@hku.hk',
+      download_url = 'https://github.com/smarkov/skpar/archive/{}.tar.gz'.format(version),
+      author='Stanislav Markov, The University of Hong Kong',
+      author_email='figaro@hku.hk',
       keywords=['dftb', 'slater-koster integrals', 'dftb+', 'lodestar', 'particle swarm optimisation', 'optimisation', 'pso', 'skpar'],
       license='MIT',
       platforms=['any'],
@@ -59,16 +54,21 @@ setup(name=name,
       package_data=package_data,
       scripts=scripts,
       data_files=data_files,
-#      classifiers=[
-#        'Development Status :: 0 - initial/exploratory',
-#        'Intended Audience :: Developers',
-#        'Intended Audience :: Education',
-#        'Intended Audience :: Science/Research',
-#        'Environment :: Console',
-#        'Operating System :: OS Independent',
-#        'License :: OSI Approved :: MIT',
-#        'Programming Language :: Python :: 3',
-#        'Topic :: Scientific/Engineering',
-#        'Topic :: Software :: Development',
-#        ],
+      install_requires = [
+            'numpy',
+            'deap',
+            'pyyaml',
+            'matplotlib'
+          ],
+      classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Environment :: Console',
+        'Operating System :: OS Independent',
+        'License :: OSI Approved :: MIT',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Software :: Development',
+        ],
 )
